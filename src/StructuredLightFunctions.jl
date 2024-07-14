eb2f5e0432de9a0a31728671f6c6d8fb7d5daa8d
LSGA001 <35610053+lsga001@users.noreply.github.com>
2024-07-14 17:15:15 -0600

    Project has correct dependencies now. Doesn't fail trivial tests.

--- src/StructuredLightFunctions.jl
+++ src/StructuredLightFunctions.jl
@@ -1,10 +1,8 @@
 module StructuredLightFunctions
 
-export LaguerreGauss
-
 using BeamAmplitudeFunctions
 
-println(AmplitudeDistributions.LaguerreGauss(0,0,0,1,1,1))
-println(AmplitudeDistributions.HermiteGauss(0,0,0,1,1,1))
+# Export relevant modules
+export AmplitudeDistributions
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using StructuredLightFunctions
 using Test
 
 @testset "StructuredLightFunctions.jl" begin
+    @test AmplitudeDistributions.LaguerreGauss(0,0,0,1,1,1)==0+0im
     # Write your tests here.
 end
